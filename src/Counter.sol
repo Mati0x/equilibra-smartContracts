8124611d8be6932ad5bec7fae84024d75e95a304
Matias <mantitaaa89@gmail.com>
2023-10-10 20:06:16 -0300

    chore: add oz upgradable lib

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,6 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
 contract Counter {
     uint256 public number;
