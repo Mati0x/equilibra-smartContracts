a4ff14c163dc35a99bc2583ccb42ab04378fd528
Matias <mantitaaa89@gmail.com>
2023-10-11 14:11:20 -0300

    chore: add ProjectRegistry.sol +  IProjectList.sol

---
+++ src/interfaces/IProjectList.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+error ProjectAlreadyInList(uint256 projectId);
+error ProjectDoesNotExist(uint256 projectId);
+error ProjectNotInList(uint256 projectId);
+
+struct Project {
+    address admin;
+    address beneficiary;
+    bytes contenthash;
+}
+
+interface IProjectList {
+    function getProject(
+        uint256 _projectId
+    ) external view returns (Project memory);
+
+    function projectExists(uint256 _projectId) external view returns (bool);
+}
