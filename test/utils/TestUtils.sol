2f0bf3163bcc3342d5c235480b0ea0fcec600464
dzkita <146095028+dzkita@users.noreply.github.com>
2023-10-21 14:22:26 -0300

    Delete test directory
    As done with src, big changes were made so, in the next commit, all tests are gonna be uploaded

--- test/utils/TestUtils.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.17;
-
-import {FormulaUtils} from "./FormulaUtils.sol";
-import {ProjectUtils} from "./ProjectUtils.sol";
-import {ProxyUtils} from "./ProxyUtils.sol";
-
-abstract contract TestUtils is FormulaUtils, ProjectUtils, ProxyUtils {}
